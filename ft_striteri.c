92757465741d246221f46129d677dedd6db30684
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2025-04-13 20:15:37 +0200

    Added function ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -2,3 +2,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+	return ;
+}
+/*
+void to_upper(unsigned int i, char *c)
+{
+	if (*c >= 'a' && *c <= 'z')
+		*c = *c - 32;
+}
+
+int main()
+{
+	char str[] = "Hello Madrid!";
+
+	printf("Before: \n%s\n", str);
+	ft_striteri(str, to_upper);
+	printf("After:\n%s\n", str);
+	return (0);
+}
+*/
